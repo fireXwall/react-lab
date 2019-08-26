2f8718a28bfd6782c4556fa9b28210c8870e36e5
unknown <djfabiokk@gmail.com>
2019-08-25 20:42:06 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:'flex', justifyContent: 'center'}}>
+      <div style={{display: "flex", flexDirection: 'column',width: '10vw', marginTop: '15vh'}}>
+        <h4>username</h4>
+        <input type='text' style={{marginBottom:'10px'}} value={username} onChange={e=>setUsername(e.target.value)} />
+        <h4>password</h4>
+        <input type='password' value={password} onChange={e=>setPassword(e.target.value)} />
+
+        <button>Log in</button>
+      </div>
     </div>
   );
 }
